d7677986a8c9db46b42cededc41d8a9a1cdbceb1
Sam Zagrobelny <strangesast@gmail.com>
2017-03-24 07:47:04 -0400

    Added promises, reworked indexeddb / create tree, added more tests

--- mixins/create-tree.js
+++ mixins/create-tree.js
@@ -5,144 +5,57 @@ var modes = require('../lib/modes.js');
 module.exports = function (repo) {
   repo.createTree = createTree;
 
-  function createTree(entries, callback) {
-    if (!callback) return createTree.bind(null, entries);
-    callback = singleCall(callback);
+  async function createTree(entries) {
     if (!Array.isArray(entries)) {
-      entries = Object.keys(entries).map(function (path) {
-        var entry = entries[path];
-        entry.path = path;
-        return entry;
-      });
+      entries = Object.keys(entries).map((path) => Object.assign(entries[path], { path }));
     }
 
-    // Tree paths that we need loaded
-    var toLoad = {};
-    function markTree(path) {
-      while(true) {
-        if (toLoad[path]) return;
-        toLoad[path] = true;
-        trees[path] = {
-          add: [],
-          del: [],
-          tree: {}
-        };
-        if (!path) break;
-        path = path.substring(0, path.lastIndexOf("/"));
+    let tree = entries.base && await repo.loadAs('tree', entries.base) || {};
+    let blobs = {};
+
+    for (let entry of entries) {
+      let fullpath = entry.path;
+      let i = fullpath.lastIndexOf('/');
+      let [path, fname] = [fullpath.substring(0, i).split('/'), fullpath.substring(i + 1)];
+      let prev = tree;
+      for (let dirname of path) {
+        let ob = prev[dirname];
+        console.log('dirname', dirname, 'ob', ob);
+        if (ob && ob.mode !== modes.tree) throw new Error('file not folder');
+        if (ob && ob.mode && ob.hash) {
+          prev = prev[dirname] = await repo.loadAs('tree', ob.hash);
+        } else if (dirname != '') {
+          prev = prev[dirname] = (prev[dirname] || {});
+        }
+        // if dirname == '', in root so don't change prev
+      }
+
+      // added / modified
+      if (entry.mode) {
+        prev[fname] = fullpath;
+        blobs[fullpath] = (async function({ mode, content }) {
+          let type = modes.toType(mode);
+          return { hash: await repo.saveAs(type, content), mode };
+        })(entry);
+      }
+      // removed
+      else {
+        delete prev[fname];
       }
     }
 
-    // Commands to run organized by tree path
-    var trees = {};
+    console.log('tree', tree);
 
-    // Counter for parallel I/O operations
-    var left = 1; // One extra counter to protect again zalgo cache callbacks.
+    return collapse(tree, blobs)
+  }
 
-    // First pass, stubs out the trees structure, sorts adds from deletes,
-    // and saves any inline content blobs.
-    entries.forEach(function (entry) {
-      var index = entry.path.lastIndexOf("/");
-      var parentPath = entry.path.substr(0, index);
-      var name = entry.path.substr(index + 1);
-      markTree(parentPath);
-      var tree = trees[parentPath];
-      var adds = tree.add;
-      var dels = tree.del;
-
-      if (!entry.mode) {
-        dels.push(name);
-        return;
-      }
-      var add = {
-        name: name,
-        mode: entry.mode,
-        hash: entry.hash
-      };
-      adds.push(add);
-      if (entry.hash) return;
-      left++;
-      repo.saveAs("blob", entry.content, function (err, hash) {
-        if (err) return callback(err);
-        add.hash = hash;
-        check();
-      });
-    });
-
-    // Preload the base trees
-    if (entries.base) loadTree("", entries.base);
-
-    // Check just in case there was no IO to perform
-    check();
-
-    function loadTree(path, hash) {
-      left++;
-      delete toLoad[path];
-      repo.loadAs("tree", hash, function (err, tree) {
-        if (err) return callback(err);
-        trees[path].tree = tree;
-        Object.keys(tree).forEach(function (name) {
-          var childPath = path ? path + "/" + name : name;
-          if (toLoad[childPath]) loadTree(childPath, tree[name].hash);
-        });
-        check();
-      });
+  async function collapse(root, blobs) {
+    for (let name in root) {
+      let val = root[name];
+      if (val.mode && val.hash) continue;
+      root[name] = typeof val == 'string' ? await blobs[val] : { hash: await collapse(root[name], blobs), mode: modes.tree };
     }
-
-    function check() {
-      if (--left) return;
-      findLeaves().forEach(processLeaf);
-    }
-
-    function processLeaf(path) {
-      var entry = trees[path];
-      delete trees[path];
-      var tree = entry.tree;
-      entry.del.forEach(function (name) {
-        delete tree[name];
-      });
-      entry.add.forEach(function (item) {
-        tree[item.name] = {
-          mode: item.mode,
-          hash: item.hash
-        };
-      });
-      left++;
-      repo.saveAs("tree", tree, function (err, hash, tree) {
-        if (err) return callback(err);
-        if (!path) return callback(null, hash, tree);
-        var index = path.lastIndexOf("/");
-        var parentPath = path.substring(0, index);
-        var name = path.substring(index + 1);
-        trees[parentPath].add.push({
-          name: name,
-          mode: modes.tree,
-          hash: hash
-        });
-        if (--left) return;
-        findLeaves().forEach(processLeaf);
-      });
-    }
-
-    function findLeaves() {
-      var paths = Object.keys(trees);
-      var parents = {};
-      paths.forEach(function (path) {
-        if (!path) return;
-        var parent = path.substring(0, path.lastIndexOf("/"));
-        parents[parent] = true;
-      });
-      return paths.filter(function (path) {
-        return !parents[path];
-      });
-    }
+    console.log('root', root);
+    return repo.saveAs('tree', root);
   }
 };
-
-function singleCall(callback) {
-  var done = false;
-  return function () {
-    if (done) return console.warn("Discarding extra callback");
-    done = true;
-    return callback.apply(this, arguments);
-  };
-}
--- mixins/indexed-db.js
+++ mixins/indexed-db.js
@@ -9,139 +9,165 @@ var db;
 mixin.init = init;
 
 mixin.loadAs = loadAs;
+mixin.loadManyRaw = loadManyRaw;
 mixin.saveAs = saveAs;
+mixin.loadRaw = loadRaw;
 module.exports = mixin;
 
-function init(callback) {
+function init(name, version) {
+  return new Promise((resolve, reject) => {
+    db = null;
+    let request = indexedDB.open(name, version);
 
-  db = null;
-  var request = indexedDB.open("tedit", 1);
+    // We can only create Object stores in a versionchange transaction.
+    request.onupgradeneeded = (evt) => {
+      db = evt.target.result;
 
-  // We can only create Object stores in a versionchange transaction.
-  request.onupgradeneeded = function(evt) {
-    var db = evt.target.result;
+      if (evt.dataLoss && evt.dataLoss !== 'none') {
+        return reject(new Error(evt.dataLoss + ': ' + evt.dataLossMessage));
+      }
 
-    if (evt.dataLoss && evt.dataLoss !== "none") {
-      return callback(new Error(evt.dataLoss + ": " + evt.dataLossMessage));
-    }
+      // A versionchange transaction is started automatically.
+      evt.target.transaction.onerror = (evt) => {
+        reject(evt.target.error);
+      };
 
-    // A versionchange transaction is started automatically.
-    evt.target.transaction.onerror = onError;
+      let storeNames = [].slice.call(db.objectStoreNames);
+      if (storeNames.indexOf('objects') != -1) {
+        db.deleteObjectStore('objects');
+      }
+      if (storeNames.indexOf('refs') != -1) {
+        db.deleteObjectStore('refs');
+      }
 
-    if(db.objectStoreNames.contains("objects")) {
-      db.deleteObjectStore("objects");
-    }
-    if(db.objectStoreNames.contains("refs")) {
-      db.deleteObjectStore("refs");
-    }
+      db.createObjectStore('objects', { keyPath: 'hash' });
+      db.createObjectStore('refs', { keyPath: 'path' });
+    };
 
-    db.createObjectStore("objects", {keyPath: "hash"});
-    db.createObjectStore("refs", {keyPath: "path"});
-  };
-
-  request.onsuccess = function (evt) {
-    db = evt.target.result;
-    callback();
-  };
-  request.onerror = onError;
+    request.onsuccess = (evt) => resolve(db = evt.target.result);
+    request.onerror = (evt) => reject(evt.target.error);
+  });
 }
 
 
 function mixin(repo, prefix) {
-  if (!prefix) throw new Error("Prefix required");
+  if (!prefix) throw new Error('Prefix required');
   repo.refPrefix = prefix;
   repo.saveAs = saveAs;
   repo.loadAs = loadAs;
+  repo.loadRaw = loadRaw;
+  repo.loadManyRaw = loadManyRaw;
   repo.readRef = readRef;
   repo.updateRef = updateRef;
   repo.hasHash = hasHash;
 }
 
-function onError(evt) {
-  console.error("error", evt.target.error);
-}
-
-function saveAs(type, body, callback, forcedHash) {
-  if (!callback) return saveAs.bind(this, type, body);
-  var hash;
-  try {
-    var buffer = codec.frame({type:type,body:body});
-    hash = forcedHash || sha1(buffer);
-  }
-  catch (err) { return callback(err); }
-  var trans = db.transaction(["objects"], "readwrite");
-  var store = trans.objectStore("objects");
-  var entry = { hash: hash, type: type, body: body };
-  var request = store.put(entry);
-  request.onsuccess = function() {
-    // console.warn("SAVE", type, hash);
-    callback(null, hash, body);
-  };
-  request.onerror = function(evt) {
-    callback(new Error(evt.value));
-  };
-}
-
-function loadAs(type, hash, callback) {
-  if (!callback) return loadAs.bind(this, type, hash);
-  loadRaw(hash, function (err, entry) {
-    if (!entry) return callback(err);
-    if (type !== entry.type) {
-      return callback(new TypeError("Type mismatch"));
+function saveAs(type, body, forcedHash) {
+  return new Promise((resolve, reject) => {
+    let hash;
+    try {
+      let buffer = codec.frame({type:type,body:body});
+      hash = forcedHash || sha1(buffer);
+    } catch (err) {
+      return reject(err);
     }
-    callback(null, entry.body, hash);
+    let trans = db.transaction(['objects'], 'readwrite');
+    let store = trans.objectStore('objects');
+    let entry = { hash: hash, type: type, body: body };
+    let request = store.put(entry);
+
+    request.onsuccess = () => resolve(hash);
+    request.onerror = (evt) => reject(evt.target.error);
   });
 }
 
-function loadRaw(hash, callback) {
-  var trans = db.transaction(["objects"], "readwrite");
-  var store = trans.objectStore("objects");
-  var request = store.get(hash);
-  request.onsuccess = function(evt) {
-    var entry = evt.target.result;
-    if (!entry) return callback();
-    return callback(null, entry);
-  };
-  request.onerror = function(evt) {
-    callback(new Error(evt.value));
-  };
+async function loadAs(type, hash) {
+  let entry = await loadRaw(hash);
+  if (!entry) return null;
+  if (type !== entry.type) {
+    throw new TypeError('Type mismatch');
+  }
+  return entry.body;
 }
 
-function hasHash(hash, callback) {
-  if (!callback) return hasHash.bind(this, hash);
-  loadRaw(hash, function (err, body) {
-    if (err) return callback(err);
-    return callback(null, !!body);
+async function loadRaw(hash) {
+  return new Promise((resolve, reject) => {
+    let trans = db.transaction(['objects'], 'readwrite');
+    let store = trans.objectStore('objects');
+    let request = store.get(hash);
+
+    request.onsuccess = (evt) => resolve(evt.target.result);
+    request.onerror = (evt) => reject(evt.target.error);
   });
 }
 
-function readRef(ref, callback) {
-  if (!callback) return readRef.bind(this, ref);
-  var key = this.refPrefix + "/" + ref;
-  var trans = db.transaction(["refs"], "readwrite");
-  var store = trans.objectStore("refs");
-  var request = store.get(key);
-  request.onsuccess = function(evt) {
-    var entry = evt.target.result;
-    if (!entry) return callback();
-    callback(null, entry.hash);
-  };
-  request.onerror = function(evt) {
-    callback(new Error(evt.value));
-  };
+function comparer(a, b) {
+  return a < b ? -1 : a > b ? -1 : 0;
 }
 
-function updateRef(ref, hash, callback) {
-  if (!callback) return updateRef.bind(this, ref, hash);
-  var key = this.refPrefix + "/" + ref;
-  var trans = db.transaction(["refs"], "readwrite");
-  var store = trans.objectStore("refs");
-  var entry = { path: key, hash: hash };
-  var request = store.put(entry);
-  request.onsuccess = function() {
-    callback();
-  };
-  request.onerror = function(evt) {
-    callback(new Error(evt.value));
-  };
+async function loadManyRaw(hashes) {
+  return new Promise((resolve, reject) => {
+    // algorithm by dfahlander
+    let set = hashes.slice().sort(comparer);
+    let request = db.transaction(['objects'], 'readonly')
+      .objectStore('objects')
+      .openCursor();
+    let i = 0;
+    let results = [];
+
+    request.onsuccess = (evt) => {
+      let cursor = evt.target.result;
+      if (!cursor) return resolve(results);
+
+      let key = cursor.key;
+
+      while (key > set[i]) {
+        ++i;
+
+        if (i === set.length) return resolve(results);
+      }
+
+      if (key === set[i]) {
+        results.push(cursor.value);
+        cursor.continue();
+
+      } else {
+        cursor.continue(set[i]);
+      }
+    };
+    request.onerror = (evt) => reject(evt.target.error);
+  });
+}
+
+async function hasHash(hash) {
+  let body = await loadRaw(hash);
+  return !!body;
+}
+
+function readRef(ref) {
+  return new Promise((resolve, reject) => {
+    let key = this.refPrefix + '/' + ref;
+    let trans = db.transaction(['refs'], 'readwrite');
+    let store = trans.objectStore('refs');
+    let request = store.get(key);
+
+    request.onsuccess = (evt) => {
+      let entry = evt.target.result;
+      resolve(entry && entry.hash);
+    };
+    request.onerror = (evt) => reject(evt.target.error);
+  });
+}
+
+function updateRef(ref, hash) {
+  return new Promise((resolve, reject) => {
+    let key = this.refPrefix + '/' + ref;
+    let trans = db.transaction(['refs'], 'readwrite');
+    let store = trans.objectStore('refs');
+    let entry = { path: key, hash: hash };
+    let request = store.put(entry);
+
+    request.onsuccess = () => resolve();
+    request.onerror = (evt) => reject(evt.target.error);
+  });
 }
